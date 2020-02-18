3b9762d4e07f3780066409bc35a6860c7eca350d
Tung Phan <nlds.a279@gmail.com>
2020-02-18 14:49:54 +0700

    Change Lineage oper

--- dags/example_lineage_operator.py
+++ dags/example_lineage_operator.py
@@ -1,11 +1,7 @@
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.lineage.datasets import File
 from airflow.models import DAG
 from airflow.utils.dates import days_ago
 from datetime import timedelta
-
-FILE_CATEGORIES = ["CAT1", "CAT2", "CAT3"]
 
 args = {
     'owner': 'Airflow',
@@ -19,20 +15,11 @@ dag = DAG(
     catchup=False
 )
 
-f_final = File("/tmp/final")
-run_this_last = DummyOperator(task_id='run_this_last', dag=dag,
-    inlets={"auto": True},
-    outlets={"datasets": [f_final,]})
-
-f_in = File("/tmp/whole_directory/")
-outlets = []
-for file in FILE_CATEGORIES:
-    f_out = File("/tmp/{}/{{{{ execution_date }}}}".format(file))
-    outlets.append(f_out)
+holy = { 'database': 'airflow', 'schema': 'airflow', 'cluster': 'gold', 'table': 'holy' }
+sensei = { 'database': 'airflow', 'schema': 'airflow', 'cluster': 'gold', 'table': 'sensei' }
 
 run_this = BashOperator(
     task_id='run_me_first', bash_command='echo 1', dag=dag,
-    inlets={"datasets": [f_in,]},
-    outlets={"datasets": outlets}
+    inlets={"datasets": [holy]},
+    outlets={"datasets": [sensei]}
 )
-run_this.set_downstream(run_this_last)
